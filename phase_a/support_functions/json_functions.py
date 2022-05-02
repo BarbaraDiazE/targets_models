ef8ed4e71f55335a551353fdc812314a61da6630
Barbara Diaz <debi_1223@hotmail.com>
2022-05-01 22:23:22 -0500

    metrics results

--- phase_a/support_functions/json_functions.py
+++ phase_a/support_functions/json_functions.py
@@ -1,8 +1,25 @@
 import os
 
 
+def read_id_json(json_root, json_filename):
+    import json
+
+    f = open(os.path.join(json_root, json_filename))
+    data_dict = json.load(f)
+    return data_dict
+
+
 def read_json(json_root, json_filename):
     import json
     f = open(os.path.join(json_root, json_filename))
     data_dict = json.load(f)
     return data_dict
+
+
+if __name__ == "__main__":
+    data = read_json(
+        "/home/babs/Documents/DIFACQUIM/targets_models/phase_a/results/trained_results/id_models",
+        "models_id.json"
+    )
+    for key, value in data.items():
+        print(value)
